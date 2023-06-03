4bf166d16a1b2dfe83abf7bc492216a6466489e5
Fab.16 <fab_16@hotmail.fr>
2023-06-03 16:47:45 +0200

    feat: :sparkles: changement du header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -5,6 +5,11 @@ export function Header() {
   return (
     <header className={style.header}>
       <Navbar bg='dark' variant='dark' className={style.nav}>
+      <span className={style.reseau}> 
+          <a href="https://discord.gg/pgEUk9xVKe" title="https://discord.gg/pgEUk9xVKe"> <img src="../../../discord.png" alt="discord"/> </a>
+          <a href="https://medium.com/@overridecommunuty" title="https://medium.com/@overridecommunuty"> <img src="../../../medium.png" alt="medium"/> </a> 
+          <a href="https://www.youtube.com/@Fab.16" title="https://www.youtube.com/@Fab.16"> <img src="../../../youtube.png" alt="youtube"/> </a> 
+      </span> 
         <Container>
           <Navbar.Brand href='/'>Prog En Images</Navbar.Brand>
         </Container>
