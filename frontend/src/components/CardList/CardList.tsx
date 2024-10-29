392e127d9d44a621b463f023f5f2ea260af3b47b
Louvet Thomas <thomas.louvet@laposte.net>
2024-10-29 19:10:06 +0100

    refactor: :recycle: ditched bootstrap for single grid

--- frontend/src/components/CardList/CardList.tsx
+++ frontend/src/components/CardList/CardList.tsx
@@ -1,30 +1,26 @@
-import { useContext } from "react";
-import { Row, Col } from "react-bootstrap";
-import { CARDLIST } from "../../data/cardlist";
-import style from "./CardList.module.scss";
-import { AppContext } from "../../context/AppContext";
-import { filterByCategory, filterBySearchInput } from "./utils";
-import { ImageCard } from "../../ui/ImageCard/";
+import { useContext } from 'react';
+import { CARDLIST } from '../../data/cardlist';
+import style from './CardList.module.scss';
+import { AppContext } from '../../context/AppContext';
+import { filterByCategory, filterBySearchInput } from './utils';
+import { ImageCard } from '../../ui/ImageCard/';
 
 export function CardList() {
   const { selectedCategory, search } = useContext(AppContext);
 
   const filteredByCategorieCards = filterByCategory(CARDLIST, selectedCategory);
-  const filteredByResearchInputCards = filterBySearchInput(
-    filteredByCategorieCards,
-    search
-  );
+  const filteredByResearchInputCards = filterBySearchInput(filteredByCategorieCards, search);
 
   return (
     <section>
       <h2>Cartes</h2>
-      <Row className={style.cardList}>
+      <ul className={style.cardList}>
         {filteredByResearchInputCards.map((card) => (
-          <Col key={card.id} xs={12} sm={6} lg={4}>
+          <li key={card.id}>
             <ImageCard card={card} />
-          </Col>
+          </li>
         ))}
-      </Row>
+      </ul>
     </section>
   );
 }
