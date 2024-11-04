7dd50cac8afc7a306ab1cc48b487f73012705008
Louvet Thomas <thomas.louvet@laposte.net>
2024-11-05 00:08:33 +0100

    refactor: :recycle: fix import missing and export

--- frontend/src/components/Filters/Filters.tsx
+++ frontend/src/components/Filters/Filters.tsx
@@ -1,13 +1,13 @@
-import { CARDLIST } from "../../data/cardlist";
-import { useMemo, useContext } from "react";
-import { formatCategories } from "./utils";
-import { Form, Row, Col } from "react-bootstrap";
-import { AppContext } from "../../context/AppContext";
-import { Select } from "../../ui/Select/Select";
-import { Locale, LocaleContext } from "../../context/LocaleContext";
-import style from "./Filters.module.scss";
-import contents from "../../contents.json";
-import { Input } from "../../ui/Input";
+import { CARDLIST } from '../../data/cardlist';
+import { useMemo, useContext } from 'react';
+import { formatCategories } from './utils';
+import { Form, Row, Col } from 'react-bootstrap';
+import { AppContext, SortValues } from '../../context/AppContext';
+import { Select } from '../../ui/Select/Select';
+import { Locale, LocaleContext } from '../../context/LocaleContext';
+import style from './Filters.module.scss';
+import contents from '../../contents.json';
+import { Input } from '../../ui/Input';
 
 export function Filters() {
   const categories = useMemo(() => formatCategories(CARDLIST), []);
@@ -16,31 +16,27 @@ export function Filters() {
   const contentsLocale = contents[locale as Locale];
 
   return (
-      <section className={style.filters}>
-        <h2>{contentsLocale.filters.title}</h2>
-        <Row>
-          <Col xs={12} md={4}>
-            <Select
-                label={contentsLocale.filters.labels.category}
-                options={categories}
-                onChange={setSelectedCategory}
-            />
-          </Col>
-          <Col xs={12} md={4}>
-            <Form.Label>Trier par: </Form.Label>
-            <Form.Select defaultValue='A-Z' onChange={(e) => setSelectedSort(e.target.value as SortValues)}>
-              <option value='A-Z'>Ordre alphabétique (A-Z)</option>
-              <option value='Z-A'>Ordre désalphabetique (Z-A)</option>
-            </Form.Select>
-          </Col>
-          <Col xs={12} md={4}>
-            <Input
-                onChange={setSearch}
-                label={contentsLocale.filters.labels.search}
-                helpertext={contentsLocale.filters.searchHelp}
-            />
-          </Col>
-        </Row>
-      </section>
+    <section className={style.filters}>
+      <h2>{contentsLocale.filters.title}</h2>
+      <Row>
+        <Col xs={12} md={4}>
+          <Select label={contentsLocale.filters.labels.category} options={categories} onChange={setSelectedCategory} />
+        </Col>
+        <Col xs={12} md={4}>
+          <Form.Label>Trier par: </Form.Label>
+          <Form.Select defaultValue='A-Z' onChange={(e) => setSelectedSort(e.target.value as SortValues)}>
+            <option value='A-Z'>Ordre alphabétique (A-Z)</option>
+            <option value='Z-A'>Ordre désalphabetique (Z-A)</option>
+          </Form.Select>
+        </Col>
+        <Col xs={12} md={4}>
+          <Input
+            onChange={setSearch}
+            label={contentsLocale.filters.labels.search}
+            helpertext={contentsLocale.filters.searchHelp}
+          />
+        </Col>
+      </Row>
+    </section>
   );
 }
--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -1,44 +1,41 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useMemo, useState } from 'react';
 
-type SortValues = "A-Z" | "Z-A";
+export type SortValues = 'A-Z' | 'Z-A';
 
 type AppContextType = {
-    selectedCategory: string;
-    setSelectedCategory: (category: string) => void;
-    selectedSort: SortValues; // Pour le tri
-    setSelectedSort: (sort: SortValues) => void;
-    search: string;
-    setSearch: (search: string) => void;
+  selectedCategory: string;
+  setSelectedCategory: (category: string) => void;
+  selectedSort: SortValues; // Pour le tri
+  setSelectedSort: (sort: SortValues) => void;
+  search: string;
+  setSearch: (search: string) => void;
 };
 
-
 export const AppContext = createContext<AppContextType>({
-    selectedCategory: "",
-    setSelectedCategory: (_: string) => null,
-    selectedSort: "A-Z", // Valeur par défaut pour le tri
-    setSelectedSort: (_: SortValues) => null,
-    search: "",
-    setSearch: (_: string) => null,
+  selectedCategory: '',
+  setSelectedCategory: (_: string) => null,
+  selectedSort: 'A-Z', // Valeur par défaut pour le tri
+  setSelectedSort: (_: SortValues) => null,
+  search: '',
+  setSearch: (_: string) => null,
 });
 
 export function AppContextProvider({ children }: any) {
-    const [selectedCategory, setSelectedCategory] = useState("");
-    const [selectedSort, setSelectedSort] = useState<"A-Z" | "Z-A">("A-Z"); // Valeur par défaut
-    const [search, setSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedSort, setSelectedSort] = useState<'A-Z' | 'Z-A'>('A-Z'); // Valeur par défaut
+  const [search, setSearch] = useState('');
 
-    const initialValues = useMemo(
-        () => ({
-            selectedCategory,
-            setSelectedCategory,
-            selectedSort,
-            setSelectedSort,
-            search,
-            setSearch,
-        }),
-        [selectedCategory, selectedSort, search]
-    );
+  const initialValues = useMemo(
+    () => ({
+      selectedCategory,
+      setSelectedCategory,
+      selectedSort,
+      setSelectedSort,
+      search,
+      setSearch,
+    }),
+    [selectedCategory, selectedSort, search]
+  );
 
-    return (
-        <AppContext.Provider value={initialValues}>{children}</AppContext.Provider>
-    );
+  return <AppContext.Provider value={initialValues}>{children}</AppContext.Provider>;
 }
